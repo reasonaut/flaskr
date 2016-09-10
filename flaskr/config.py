f0c182578555a83ee76e7d834bded3d2c0ae9706
reasonaut <jonnygarrity@gmail.com>
2016-09-10 18:19:31 -0500

    refactor

---
+++ flaskr/config.py
@@ -0,0 +1,16 @@
+# all the imports
+import os
+from flask import Flask
+
+
+# create our little application :)
+app = Flask(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
